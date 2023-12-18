f92b71a317e007cda9dc56dc0e80857a188c17ca
Arun <141402276+Arun10112000@users.noreply.github.com>
2023-12-18 16:03:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,8 +61,6 @@ app.post("/register", function(req, res){
 app.post("/login",function(req,res){
     const username = req.body.username;
     const password = req.body.password;
-
-    // User.findOne({email: username},function(err, foundUser){
     //     if(err){
     //       console.log(err);
     //     } else{
